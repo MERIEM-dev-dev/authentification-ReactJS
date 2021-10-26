b81925b23325bf7793230fd024c710d405b1e615
MERIEM-dev-dev <meriem.ahouzi90@gmail.com>
2021-10-26 16:36:37 +0100

    save

---
+++ src/components/My component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Car from ''
+
+
+
+class Mycomponents extends Component {
+    render() {
+        return <h1>Hello React</h1>
+        
+
+    }
+}
+
+export default Mycomponents;
---
+++ src/components/composent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const comp = () => {
+    state = (
+        return (
+            <div>
+                <p>Nom: </p>
+                <p>Prenom: </p>
+            </div>
+    )
+    
+    )
+}
+
+export default comp;
