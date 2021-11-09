061c06bd815cec90254384eaf35977ca615de677
MERIEM-dev-dev <meriem.ahouzi90@gmail.com>
2021-11-09 11:26:12 +0100

    save

--- realisation/js/Login.component.js
+++ realisation/js/Login.component.js
@@ -11,15 +11,15 @@ return (
       <h1>Login</h1>
       <div className="textbox">
         <i className="fas fa-user" />
-        <input type="text" placeholder="Username" name defaultValue />
+        <input type="text" placeholder="Username"  />
       </div>
       <div className="textbox">
         <i className="fas fa-lock" />
-        <input type="password" placeholder="Password" name defaultValue />
+        <input type="password" placeholder="Password"  />
       </div>
       {/* <a href="Home.html" type="submit">Log in</a><br><br> */}
       <button onclick="window.location.href='Home.html';">Login</button><br /><br />
-      <a href="Sign.html" type="text">Create an account</a>
+      <a href="signup.html" type="text">Create an account</a>
     </div>
   );
 
--- realisation/js/SignUp.component.js
+++ realisation/js/SignUp.component.js
@@ -12,22 +12,22 @@ class SignUp extends React.Component {
         <h1>Sign Up</h1>
         <div className="textbox">
           <i className="fas fa-user" />
-          <input type="text" placeholder="Enter Your Name" name="text" defaultValue />
+          <input type="text" placeholder="Enter Your Name" name="text"/>
         </div>
         <div className="textbox">
           <i className="fad fa-envelope" />
-          <input type="text" placeholder="Enter Email" name="email" defaultValue />
+          <input type="text" placeholder="Enter Email" name="email"  />
         </div>
         <div className="textbox">
           <i className="fas fa-lock" />
-          <input type="password" placeholder="Enter Password" name="psw" defaultValue />
+          <input type="password" placeholder="Enter Password" name="psw"/>
         </div>
         <div className="textbox">
           <i className="fas fa-lock" />
-          <input type="password" placeholder="Repeat Password" name="psw-repeat" defaultValue />
+          <input type="password" placeholder="Repeat Password" name="psw-repeat"/>
         </div>
-        <button onclick="window.location.href='Login.component.js';">Sign Up</button><br /><br />
-        <a href="../login.html" type="text">Already have an account?</a>
+        <button onclick="window.location.href='realisation\login.html';">Sign Up</button><br/><br/>
+        <a href="login.html" type="text">Already have an account?</a>
       </div>
          
          
