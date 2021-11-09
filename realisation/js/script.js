754e5efa4b9fd48f99029d1d86e0102fcebbd26f
MERIEM-dev-dev <meriem.ahouzi90@gmail.com>
2021-11-09 12:49:30 +0100

    save

--- realisation/js/script.js
+++ realisation/js/script.js
@@ -1,2 +1,16 @@
 ReactDOM.render(<SignUp />, document.getElementById('SignUp'));
 ReactDOM.render(<Login />, document.getElementById('login'));
+
+let users =
+`[
+    
+    {
+            "Enter Your Name" : "meriem",
+            "Enter Email" : "meriem@gmail.com",
+            "Enter Password" : "meriem",
+            "Repeat Password" : "meriem"
+    }
+    
+]`
+
+console.log(JSON.users)
