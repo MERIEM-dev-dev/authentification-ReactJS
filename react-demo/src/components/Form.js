a4d0ab3fd8f97b19c5e1d2ddea6b43d7596c2581
MERIEM-dev-dev <meriem.ahouzi90@gmail.com>
2021-11-10 13:22:21 +0100

    save

--- react-demo/src/components/Form.js
+++ react-demo/src/components/Form.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import Signup from './Signup';
+// import React from 'react';
+// import Signup from './Signup';
 
 
-const Form = () => {
-    return (
-        <div className="login-box">
-      <h1>Login</h1>
-      <div className="textbox">
-        <i className="fas fa-user" />
-        <input type="text" placeholder="Username"  />
-      </div>
-      <div className="textbox">
-        <i className="fas fa-lock" />
-        <input type="password" placeholder="Password"  />
-      </div>
-      {/* <a href="Home.html" type="submit">Log in</a><br><br> */}
-      <button onclick="window.location.href='Home.html';">Login</button><br /><br />
-      <a href="signup.html" type="text">Create an account</a>
-    </div>
-    );
-};
+// const Form = () => {
+//     return (
+//         <div className="login-box">
+//       <h1>Login</h1>
+//       <div className="textbox">
+//         <i className="fas fa-user" />
+//         <input type="text" placeholder="Username"  />
+//       </div>
+//       <div className="textbox">
+//         <i className="fas fa-lock" />
+//         <input type="password" placeholder="Password"  />
+//       </div>
+//       {/* <a href="Home.html" type="submit">Log in</a><br><br> */}
+//       <button onclick="window.location.href='Home.html';">Login</button><br /><br />
+//       <a href="signup.html" type="text">Create an account</a>
+//     </div>
+//     );
+// };
 
-export default Form;
+// export default Form;
