be6b5b24582373877bd6af8d7e81cfc7ea8d759f
MERIEM-dev-dev <meriem.ahouzi90@gmail.com>
2021-11-10 16:02:53 +0100

    save

--- react-demo/src/components/validation.js
+++ react-demo/src/components/validation.js
@@ -7,7 +7,12 @@ const validation = (values) => {
 
     if(!values.email) {
         errors.email="Email is required."
+    } else if(!/\S+@\S+\.\S+/.test(values.email)){
+        errors.email="Email is invalid."
     }
+    if(!values.password){
+        errors.password="Password is required."
+    } else if(values.password.length < 5)
 
     return (
         <div>
