4eca81895abd725368ab2e98918c12f745ad6206
MERIEM-dev-dev <meriem.ahouzi90@gmail.com>
2021-11-10 13:41:27 +0100

    save

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,13 +1,13 @@
 import { React } from 'react';
 import './App.css';
-import Form from "./components/Form";
+// import Form from "./components/Form";
 import Signup from "./components/Signup";
 
 
 function App () {
   return (
     <div className="App">
-    <Form />
+    {/* <Form /> */}
     <Signup />
     </div>
   );
