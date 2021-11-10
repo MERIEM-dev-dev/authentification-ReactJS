6d9921fc01cd1934ab5235012f1669c0c444d320
MERIEM-dev-dev <meriem.ahouzi90@gmail.com>
2021-11-10 10:20:12 +0100

    save

--- react-demo/src/components/Form.js
+++ react-demo/src/components/Form.js
@@ -3,10 +3,21 @@ import Signup from './Signup';
 
 const Form = () => {
     return (
-        <div>
-           <Signup /> 
-        </div>
-    )
-}
+        <div className="login-box">
+      <h1>Login</h1>
+      <div className="textbox">
+        <i className="fas fa-user" />
+        <input type="text" placeholder="Username"  />
+      </div>
+      <div className="textbox">
+        <i className="fas fa-lock" />
+        <input type="password" placeholder="Password"  />
+      </div>
+      {/* <a href="Home.html" type="submit">Log in</a><br><br> */}
+      <button onclick="window.location.href='Home.html';">Login</button><br /><br />
+      <a href="signup.html" type="text">Create an account</a>
+    </div>
+    );
+};
 
 export default Form;
--- react-demo/src/components/Signup.js
+++ react-demo/src/components/Signup.js
@@ -1,10 +1,33 @@
 import React from 'react'
 
 function SignupForm () {
+
+        const handleFormSubmit = (event) => {
+            event.preventDefault();
+        };
     return (
-        <div>
-            
+    <div className="login-box">
+        <h1>Sign Up</h1>
+        <div className="textbox">
+          <i className="fas fa-user" />
+          <input type="text" placeholder="Enter Your Name" name="text"/>
         </div>
+        <div className="textbox">
+          <i className="fad fa-envelope" />
+          <input type="text" placeholder="Enter Email" name="email"  />
+        </div>
+        <div className="textbox">
+          <i className="fas fa-lock" />
+          <input type="password" placeholder="Enter Password" name="psw"/>
+        </div>
+        <div className="textbox">
+          <i className="fas fa-lock" />
+          <input type="password" placeholder="Repeat Password" name="psw-repeat"/>
+        </div>
+        <button onclick= {handleFormSubmit}>Sign Up</button><br/><br/>
+        <a href="login.html" type="text">Already have an account?</a>
+    </div>
+
     )
 }
 
