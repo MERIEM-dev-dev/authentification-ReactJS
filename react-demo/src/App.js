4ac89ba5ce9a737b79297313d365899fb2a74bbd
MERIEM-dev-dev <meriem.ahouzi90@gmail.com>
2021-11-12 00:00:06 +0100

    save

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -2,7 +2,7 @@ import { React } from 'react';
 import './App.css';
 import { Router, Route, Routes } from "react-router-dom";
 // import Form from "./components/Form";
-import Data from './data.json' ;
+import Data from 'data.json' ;
 import Signup from "./components/Signup";
 
 
